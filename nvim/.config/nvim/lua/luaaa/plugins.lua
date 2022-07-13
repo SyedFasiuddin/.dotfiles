f05113c06c0e2c196a83e6f7ebd7e3f8e2725283
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2022-07-13 19:14:00 +0530

    feat: adding treesitter!!!

--- nvim/.config/nvim/lua/luaaa/plugins.lua
+++ nvim/.config/nvim/lua/luaaa/plugins.lua
@@ -26,12 +26,25 @@ return packer.startup(function()
     use "hrsh7th/nvim-cmp"      -- completion core -> popup, snippets etc
     use "hrsh7th/cmp-buffer"    -- source which gives completion from buffer
     use "L3MON4D3/LuaSnip"      -- the thing which enable the snippets from lsp to work/expand properly
+    use "saadparwaiz1/cmp_luasnip"
 
     -- lsp
     use "neovim/nvim-lspconfig" -- lsp core -> has configs to make built in lsp client to work
+    use "williamboman/nvim-lsp-installer" -- makes it easy to install language servers
 
     -- combine lsp and cmp
     use "hrsh7th/cmp-nvim-lsp"  -- thing that enables cmp to tell the language server that it can show completions and requests it to send em 
+
+    -- treesitter
+    use("nvim-treesitter/nvim-treesitter", {
+        run = ":TSUpdate"
+    })
+    use "p00f/nvim-ts-rainbow"
+
+    use {
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true }
+    }
 
     -- colorschemes
     use "luisiacc/gruvbox-baby"
