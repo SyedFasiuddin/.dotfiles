ee5da3bbbf463d7d4521404be7a5e092e0b0c7ad
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2022-07-13 19:13:26 +0530

    another minor update

--- nvim/.config/nvim/lua/luaaa/colors.lua
+++ nvim/.config/nvim/lua/luaaa/colors.lua
@@ -1,9 +1,10 @@
-local colorscheme = "gruvbox"
+local colorscheme = "gruvbox-baby"
+local status_ok
 
 
 -- To make background transparent with any color
 -- https://www.reddit.com/r/neovim/comments/3v06lo/comment/cxj6e4q/?utm_source=share&utm_medium=web2x&context=3
-local status_ok, _ = pcall(vim.cmd, "au ColorScheme * hi Normal ctermbg=none guibg=none")
+status_ok, _ = pcall(vim.cmd, "au ColorScheme * hi Normal ctermbg=none guibg=none")
 -- local status_ok, _ = pcall(vim.cmd, "au ColorScheme myspecialcolors hi Normal ctermbg=red guibg=red")
 if not status_ok then
     print("Transparent background setup failed")
@@ -22,7 +23,7 @@ vim.g.gruvbox_contrast_dark = 'hard'
 
 -- setting colorscheme here
 -- options for any colorschemes should go above this
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
     print("Colorscheme " .. colorscheme .. " notfound")
     return
