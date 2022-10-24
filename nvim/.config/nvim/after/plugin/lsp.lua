a532e09b475ec1d4f03104e7b1d14904a1236512
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2022-10-24 15:24:07 +0530

    fix: cmp deprecated it, new way added

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -25,9 +25,7 @@ if not cmp_ok then
     return
 end
 
-local capabilities = cmp_nvim_lsp.update_capabilities(
-    vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
