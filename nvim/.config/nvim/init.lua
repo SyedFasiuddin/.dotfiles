13bcd030faa69e1d2223340a227534a9dbb14ddb
Syed Fasiuddin <syed.fasiuddin@icloud.com>
2022-11-18 21:34:01 +0530

    yy highlight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,4 +2,18 @@ require("luaaa.plugins")
 require("luaaa.set")
 require("luaaa.keymaps")
 
-vim.cmd[[autocmd BufWritePre *.txt :%s/\s\+$//e]]
+vim.cmd [[autocmd BufWritePre *.txt :%s/\s\+$//e]]
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank({
+            timeout = 40,
+        })
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
+-- kickstart.nvim and ThePrimeagen
