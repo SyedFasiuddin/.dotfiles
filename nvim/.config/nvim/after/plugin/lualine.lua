b2bf89bb549c39ed7d8006a61a1bd4735201d75d
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2023-01-14 18:02:21 +0530

    fix: lualine
    - moved refresh option to correct place
    - fixed styling with stylua to make it consistent with others
    - brand new tabline
      - it is only shown when more than one tab is present
      - the `showtabline` option has to be set in this file and cannot be
        moved to set.lua file as lualine.lua is loaded after that file and
        by default lualine makes the tabline visible as there are
        configurations for it.
      - it shows tabpages with tab number and current file name (default)
        - just like tmux - window number and currently running command name
      - added `fmt` function which shows a `+` sign if there is and unsaved
        file in the tab
      - added key bind for renaming tabs, similar with tmux `<C-a>,`

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -4,35 +4,62 @@ if not status_ok then
     return
 end
 
-lualine.setup {
+lualine.setup({
     options = {
         icons_enabled = true,
         theme = "gruvbox-baby",
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
         globalstatus = true,
+        refresh = {
+            statusline = 500,
+            tabline = 500,
+            winbar = 500,
+        },
     },
     sections = {
         lualine_a = {
-            "mode"
-    --        { 'mode', fmt = function(str) return str:sub(1,2) end },
+            "mode",
+            -- { "mode", fmt = function(str) return str:sub(1,2) end },
         },
-        lualine_b = {"filename"},
-        lualine_c = {'diff', 'diagnostics'},
-        lualine_x = {"encoding", "fileformat"},
+        lualine_b = { "filename" },
+        lualine_c = { "diff", "diagnostics" },
+        lualine_x = { "encoding", "fileformat" },
         lualine_y = {},
-        lualine_z = {"filesize"},
+        lualine_z = { "filetype" },
     },
     tabline = {
-        lualine_a = {"location", "progress"},
-        lualine_z = {"tabs"},
+        lualine_a = {
+            {
+                "tabs",
+                mode = 1,
+                max_length = vim.o.columns,
+                tabs_color = {
+                    active = "lualine_c_inactive",
+                    inactive = "lualine_a_active",
+                },
+                fmt = function(name, context)
+                    local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                    local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                    local bufnr = buflist[winnr]
+                    local mod = vim.fn.getbufvar(bufnr, "&mod")
+                    return context.tabnr .. ":" .. name .. (mod == 1 and "+" or "")
+                end
+            }
+        },
     },
-    refresh = {
-        statusline = 500,
-        tabline = 500,
-        winbar = 500
-    }
-}
+})
 
-vim.opt.showtabline = 1     -- this is exactly what I wanted. showing the tabline when I have more then one tab open otherwise not showing it!!!
+vim.keymap.set("n", "<C-w>,", function()
+    vim.ui.input({
+        prompt = "Enter new tab name: ",
+    }, function(input)
+        if input then
+            vim.cmd("LualineRenameTab " .. input)
+        else
+            vim.cmd("LualineRenameTab")
+        end
+    end)
+end, { desc = "Rename tabs" })
 
+vim.opt.showtabline = 1
