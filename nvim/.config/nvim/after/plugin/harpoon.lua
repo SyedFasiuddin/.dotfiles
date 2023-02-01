f22a9f90e27c8af5206449f5bcb38a0c1fe35a4d
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2023-02-01 15:54:36 +0530

    feat: leader key binds for harpoon
    - Making protected calls to harpoon
      - just to be consistent with entire config
    - add and view of harpoon now make use of leader key
    - wrote a simple utility function that handles adding key maps
      of harpoon file switching in elegant way
      - other wise there was repeated code 4 times
      - makes it easy to add even more files
        - other wise had to copy all those lines down again.

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,10 +1,30 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local mark_ok, mark = pcall(require, "harpoon.mark")
+if not mark_ok then
+    print("Failed to load Harpoon marks")
+    return
+end
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+local ui_ok, ui = pcall(require, "harpoon.ui")
+if not ui_ok then
+    print("Failed to load Harpoon ui")
+    return
+end
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<Leader>a", mark.add_file, { desc = "Harpoon: [A]dd" })
+vim.keymap.set("n", "<Leader>w", ui.toggle_quick_menu,
+    { desc = "Harpoon: Vie[w]" })
+
+local function set_harpoon_keys(opts)
+    for key, num in pairs(opts) do
+        vim.keymap.set("n", "<Leader>" .. key, function()
+            ui.nav_file(num)
+        end, { desc = "Harpoon: File [" .. num .. "]" })
+    end
+end
+
+set_harpoon_keys({
+    ["h"] = 1,
+    ["j"] = 2,
+    ["k"] = 3,
+    ["l"] = 4,
+})
