d3627c3a677137d98eaaf7ebc52d554027fabb26
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-10 19:49:46 +0530

    fix(nvim): no more semantic token highlights

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -59,3 +59,10 @@ for _, value in pairs(servers) do
         capabilities = capabilities,
     })
 end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+});
