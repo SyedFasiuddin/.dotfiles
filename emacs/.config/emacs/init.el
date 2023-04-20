d4493f8c5a3c866b1db0bb75a6c37fb74a67f0e3
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-20 12:22:57 +0530

    feat(emacs): minimal look

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,9 @@
+;; Minimal look
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+(setq inhibit-startup-screen 1
+      make-backup-files nil
+      global-hl-line-mode t)
+(set-frame-font "SF Mono 16")
