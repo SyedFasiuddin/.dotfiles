e22a4800533541bab89c52879df3232dc891a7c3
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-22 22:18:20 +0530

    feat(emacs): better view of builtin completion

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -72,12 +72,17 @@
     (lambda ()
       (org-bullets-mode 1))))
 
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(modus-themes doom-themes evil use-package)))
+ '(package-selected-packages '(vertico modus-themes doom-themes evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
