6c4850b80a03bad8e480ace8671ffaaae1b509ba
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-04-22 21:22:39 +0530

    feat(emacs): second brain (org roam)
    Setting up package `org-roam' it is "bring roam research to Emacs by
    utilizing org-mode" thing.
    
    In mini buffer, it turns out "SPC" (space) doesn't enter space character
    so had to do that little trick here.
    
    The `custom-set-variables' section is automatically generated by Emacs
    when it downloads and sets up packages, so ignore it.

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -30,3 +30,27 @@
   (setq modus-themes-italic-constructs t
         modus-themes-bold-constructs t))
 (load-theme 'modus-operandi :no-confirm)
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/Documents/Zettelkasten/"))
+  :bind (("C-c n c" . org-roam-capture)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (define-key minibuffer-local-completion-map (kbd "SPC") 'self-insert-command)
+  (org-roam-db-autosync-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(modus-themes doom-themes evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
