9dbe7dd6f65c40ded6fd7421f34faf51acfc1f67
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-05-07 09:37:58 +0530

    feat(spell): nvim spell support for .txt and .md
    adding basic spell support for two file types
    
    using same spell file between both vim and neovim

---
+++ nvim/.config/nvim/ftplugin/text.lua
@@ -0,0 +1,14 @@
+local o = vim.opt_local
+
+o.list = true
+o.listchars:append("tab:>-")
+
+o.spell = true
+o.spelllang:append("en_us")
+
+local spellfile_en = os.getenv("HOME") .. "/.vim/spell/en.utf-8.add"
+o.spellfile:append(spellfile_en)
+
+o.dictionary:append("/usr/share/dict/words")
+
+o.textwidth = 80
