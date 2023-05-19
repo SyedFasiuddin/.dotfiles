92caf4af2b11b9b96507932d05d65a1473b76daa
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-05-19 19:40:39 +0530

    fix(nvim): press enter after command

--- nvim/.config/nvim/ftplugin/rust.lua
+++ nvim/.config/nvim/ftplugin/rust.lua
@@ -1,4 +1,4 @@
 vim.opt_local.colorcolumn = "99"
 
-vim.api.nvim_set_keymap("n", "<F3>", "<Cmd>!cargo fmt",
+vim.api.nvim_set_keymap("n", "<F3>", "<Cmd>!cargo fmt<CR>",
     { noremap = true, silent = true })
