99157a4307f16a316578d7700ef2dce45c3cedb3
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-08-27 08:01:54 +0530

    nvim: lazy load mason
    - Mason is only used when installing a language server or a formatter
      and is not needed any other time
    - So lazy loading it only when it is called otherwise just don't load it
    - Removing `mason-lspconfig' to see if something breaks and if something
      broke then will probably bring it back until then no need, actually
      the idea to remove this came from looking at config of NvChad

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,24 +1,18 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ui = {
-                    -- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
-                    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        -- package_uninstalled = "✗",
-                    },
-                },
-            })
-        end
+    "williamboman/mason.nvim",
+    cmd = {
+        "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall",
+        "MasonUninstallAll", "MasonLog"
     },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup()
-        end
-    },
+    opts = {
+        ui = {
+            -- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                -- package_uninstalled = "✗",
+            },
+        },
+    }
 }
