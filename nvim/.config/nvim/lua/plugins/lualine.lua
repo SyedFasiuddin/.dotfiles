1ca0730772fb5ec14abf793f7432e2dda980250f
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-08-27 08:29:51 +0530

    nvim: lazy load lualine
    - delaying loading of lualine after initial screen draw similar to
      treesitter and telescope
    - using the lazy way of calling setup, but I also need to keep that
      keymap around, may be use `keys' entry in this table

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,67 +1,69 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "kyazdani42/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup({
-            options = {
-                icons_enabled = true,
-                theme = "auto",
-                section_separators = { left = "", right = "" },
-                component_separators = { left = "", right = "" },
-                globalstatus = true,
-                refresh = {
-                    statusline = 500,
-                    tabline = 500,
-                    winbar = 500,
-                },
+    event = "UIEnter",
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = "auto",
+            section_separators = { left = "", right = "" },
+            component_separators = { left = "", right = "" },
+            globalstatus = true,
+            refresh = {
+                statusline = 500,
+                tabline = 500,
+                winbar = 500,
             },
-            sections = {
-                lualine_a = {
-                    { "mode", fmt = function(str) return str:sub(1, 3) end },
-                },
-                lualine_b = {
-                    {
-                        "diagnostics",
-                        symbols = { error = "", warn = "", info = "", hint = "" },
-                        always_visible = true,
-                    }
-                },
-                lualine_c = { "%f" },
-                lualine_x = {
-                    "encoding",
-                    {
-                        "fileformat",
-                        symbols = {
-                            unix = "Unix",
-                            dos = "Dos",
-                            mac = "Mac",
-                        }
-                    }
-                },
-                lualine_y = { "diff" },
-                lualine_z = { "filetype" },
+        },
+        sections = {
+            lualine_a = {
+                { "mode", fmt = function(str) return str:sub(1, 3) end },
             },
-            tabline = {
-                lualine_a = {
-                    {
-                        "tabs",
-                        mode = 1,
-                        max_length = vim.o.columns,
-                        tabs_color = {
-                            active = "lualine_c_inactive",
-                            inactive = "lualine_a_active",
-                        },
-                        fmt = function(name, context)
-                            local buflist = vim.fn.tabpagebuflist(context.tabnr)
-                            local winnr = vim.fn.tabpagewinnr(context.tabnr)
-                            local bufnr = buflist[winnr]
-                            local mod = vim.fn.getbufvar(bufnr, "&mod")
-                            return context.tabnr .. ":" .. name .. (mod == 1 and "+" or "")
-                        end
-                    }
-                },
+            lualine_b = {
+                {
+                    "diagnostics",
+                    symbols = { error = "", warn = "", info = "", hint = "" },
+                    always_visible = true,
+                }
             },
-        })
+            lualine_c = { "%f" },
+            lualine_x = {
+                "encoding",
+                {
+                    "fileformat",
+                    symbols = {
+                        unix = "Unix",
+                        dos = "Dos",
+                        mac = "Mac",
+                    }
+                }
+            },
+            lualine_y = { "diff" },
+            lualine_z = { "filetype" },
+        },
+        tabline = {
+            lualine_a = {
+                {
+                    "tabs",
+                    mode = 1,
+                    max_length = vim.o.columns,
+                    tabs_color = {
+                        active = "lualine_c_inactive",
+                        inactive = "lualine_a_active",
+                    },
+                    fmt = function(name, context)
+                        local buflist = vim.fn.tabpagebuflist(context.tabnr)
+                        local winnr = vim.fn.tabpagewinnr(context.tabnr)
+                        local bufnr = buflist[winnr]
+                        local mod = vim.fn.getbufvar(bufnr, "&mod")
+                        return context.tabnr .. ":" .. name .. (mod == 1 and "+" or "")
+                    end
+                }
+            },
+        },
+    },
+    config = function(_, opts)
+        require("lualine").setup(opts)
 
         vim.keymap.set("n", "<C-w>,", function()
             vim.ui.input({
