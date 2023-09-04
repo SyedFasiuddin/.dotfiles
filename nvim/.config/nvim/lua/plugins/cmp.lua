570b1c00bfc40ebe99cc3aef879fd2cc35174933
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-09-04 21:46:18 +0530

    fix(nvim): referning to luasnip without loading it

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -15,6 +15,7 @@ return {
     },
     config = function()
         local cmp = require("cmp")
+        local luasnip = require("luasnip")
         cmp.setup({
             snippet = {
                 expand = function(args)
