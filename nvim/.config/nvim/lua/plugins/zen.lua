a9eb6123e5489659429a0edf835e515da8b68887
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-09-05 16:10:07 +0530

    feat: zen mode for neovim, removed from vim
    setting up zen mode for neovim and removing Goyo from vim decreased a
    lot of lines from vimrc and added the plugin for neovim
    
    also using `$EDITOR' everywhere instead of `nvim' or `vim'
    
    getting rid of `pj' as it is not being used and my Todo CLI is being
    used instead
    
    all this is made possible by lazy.nvim and its quick load timing

---
+++ nvim/.config/nvim/lua/plugins/zen.lua
@@ -0,0 +1,38 @@
+return {
+    "folke/zen-mode.nvim",
+    dependencies = {
+        {
+            "folke/twilight.nvim",
+            opts = {
+                dimming = {
+                    alpha = 0.25,
+                },
+            }
+        }
+    },
+    opts = {
+        window = {
+            backdrop = 1,
+            width = 80,
+            height = .50,
+            options = {
+                number = false,
+                relativenumber = false,
+            }
+        },
+    },
+    keys = {
+        {
+            "<F2>",
+            "<Cmd> ZenMode <CR>",
+            desc = "Toggle Zen Mode",
+        },
+        {
+            "<F2>",
+            "<ESC><Cmd> ZenMode <CR>a",
+            mode = { "i" },
+            desc = "Toggle Zen Mode",
+        },
+    },
+    cmd = "ZenMode",
+}
