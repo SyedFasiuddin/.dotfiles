26143ea2325d28222e5030395de20ef98f892f68
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-01-10 18:54:02 +0530

    feat(mac): add iCloud folder to $HOME

--- macos.sh
+++ macos.sh
@@ -5,3 +5,6 @@ xcode-select --install
 
 # Set wallpaper
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Solid Colors/Stone.png"'
+
+# iCloud Folder
+ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" $HOME/iCloud
