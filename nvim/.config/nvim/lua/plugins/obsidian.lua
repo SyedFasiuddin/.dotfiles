75eeca5c20a777de8eea373eea60fa4b250306d2
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-02-04 20:51:06 +0530

    fix(nvim): obsidian note file name
    - Changed file names so that they can be easily searched, the default
      was to have time stamp + 4 random characters, by looking at which you
      would not know what the file contains.
      - By changing the filename function to this, we now have a time stamp
        plus the escaped title of note, that way it gives a context to what
        it contains and can be searched via filenames.
    - Using obsidian templates for quickly having a structure.

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -35,9 +35,23 @@ return {
                 path = "~/Documents/Zettelkasten",
             },
         },
+        note_id_func = function(title)
+            local suffix = ""
+            if title ~= nil then
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+                for _ = 1, 4 do
+                    suffix = suffix .. string.char(math.random(65, 90))
+                end
+            end
+            return tostring(os.time()) .. "-" .. suffix
+        end,
         disable_frontmatter = true,
         ui = {
             enable = false,
+        },
+        templates = {
+            subdir = "templates",
         },
     },
     keys = {
