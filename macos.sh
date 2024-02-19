71a295d45d837a9cb9b5ac47860d03af36136bca
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-02-19 17:34:40 +0530

    fix(mac): disable long key press accent chars

--- macos.sh
+++ macos.sh
@@ -8,3 +8,6 @@ osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/S
 
 # iCloud Folder
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" $HOME/iCloud
+
+# Disable showing accent characters when key held down
+defaults write -g ApplePressAndHoldEnabled -bool false
