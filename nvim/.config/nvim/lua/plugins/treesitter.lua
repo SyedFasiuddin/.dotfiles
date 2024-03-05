1a36c77df930e9146a7e9958b26d4614962a3e49
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-03-05 17:46:51 +0530

    fix(nvim): disable treesitter on big files
    Taken straight from README, also the highlighting of ledger files is
    better with vim highlighting rather than treesitter ones.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,14 @@ return {
     opts = {
         highlight = {
             enable = true,
+            disable = function(lang, buf)
+                local max_filesize = 50 * 1024 -- 50 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
+            disable = { "ledger" },
             additional_vim_regex_highlighting = false,
         },
         indent = { enable = true },
