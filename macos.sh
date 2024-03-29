e58e03573b0d33d1d9f5a977f37d58c91cce2d4f
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-03-29 15:19:37 +0530

    feat: change colors of alacritty with system
    Used <https://github.com/bouk/dark-mode-notify> to prepare a login item
    that will call a script, within the script it provides an environment
    variable `DARKMODE` using whose value we can determine what we need to
    do.
    
    Using this we change the color scheme of alacritty. (NOTE: alacritty
    will reload configuration changes when the file changes)

--- macos.sh
+++ macos.sh
@@ -11,3 +11,7 @@ ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs" $HOME/iCloud
 
 # Disable showing accent characters when key held down
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# See: https://github.com/bouk/dark-mode-notify
+cp -v ke.bou.dark-mode-notify.plist ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/ke.bou.dark-mode-notify.plist
