d4b534f1339d7e2961c253c8b19323c904bb454f
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-04-30 22:23:34 +0530

    feat: bootstrap home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,20 @@
   description = ".dotfiles (everything)";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { nixpkgs, ... }: {
-  };
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."syedfasiuddin" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
 }
