592240f7dc390c96f9abb4cdd337e856428b0e7d
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-06-13 17:57:52 +0530

    fix(nix): incorrect channel usage by home-manager

--- home.nix
+++ home.nix
@@ -6,16 +6,9 @@ in
 {
   home.username = user;
   home.homeDirectory = "/Users/${user}";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
   xdg.enable = true;
+  news.display = "silent";
 
   home.packages = with pkgs; [
     abduco
@@ -72,6 +65,7 @@ in
     };
     fzf.enable = true;
     zoxide.enable = true;
+    home-manager.enable = true;
   };
 
   home.file = {
@@ -112,7 +106,4 @@ in
       recursive = true;
     };
   };
-
-  programs.home-manager.enable = true;
-  news.display = "silent";
 }
