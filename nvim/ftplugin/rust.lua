4ddf7a0d24d5dba6cc6c4c1d5a024e8769bb43f7
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-07-10 20:13:20 +0530

    fix(nvim): compiler being overridden

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,6 +1,4 @@
 vim.opt_local.colorcolumn = "99"
 
-vim.api.nvim_set_keymap("n", "<F3>", "<Cmd>!cargo fmt<CR>",
+vim.api.nvim_set_keymap("n", "<F3>", "<Cmd>silent! cargo fmt<CR>",
     { noremap = true, silent = true })
-
-vim.cmd.compiler("rustc")
